ce4dd80022e124db3f04d0d9bf4cbe0dcd9a0ec8
TevenV27 <victoria.kevin@correounivalle.edu.co>
2023-07-16 23:54:28 -0500

    Maps

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import '../stylesheets/Maps.css'
+import { ClipLoader } from 'react-spinners';
+import '../stylesheets/Maps.css';
 
 const Tarjet = ({ map }) => {
   return (
@@ -12,6 +13,7 @@ const Tarjet = ({ map }) => {
 
 export default function Maps() {
   const [maps, setMaps] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://valorant-api.com/v1/maps', {
@@ -20,17 +22,29 @@ export default function Maps() {
       }
     })
       .then(res => res.json())
-      .then(data => setMaps(data.data))
+      .then(data => {
+        // Simular un retraso de 2 segundos antes de cambiar el estado loading a false
+        setTimeout(() => {
+          setMaps(data.data);
+          setLoading(false);
+        }, 700);
+      })
       .catch(err => console.log(err));
   }, []);
 
   return (
     <div className="container-map">
-      {maps.map(map => (
-        <React.Fragment key={map.uuid}>
-          {map.uuid && <Tarjet map={map} />}
-        </React.Fragment>
-      ))}
+      {loading ? (
+        <div className="loader-container">
+          <ClipLoader color={'rgb(242,28,13)'} loading={loading} size={70} />
+        </div>
+      ) : (
+        maps.map(map => (
+          <React.Fragment key={map.uuid}>
+            {map.uuid && <Tarjet map={map} />}
+          </React.Fragment>
+        ))
+      )}
     </div>
   );
 }
